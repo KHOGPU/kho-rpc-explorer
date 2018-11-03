7fad22ff5a270c372e3ddc4911c7e35ed458e26c
xDk <incend@gmail.com>
2018-11-03 11:34:45 +0100

    rpcport update

--- app/credentials.js
+++ app/credentials.js
@@ -1,13 +1,13 @@
 module.exports = {
-	
+
 	// Edit "rpc" below to target your node.
 	// You may delete this section if you wish to connect manually via the UI.
 
 	rpc: {
 		host:"localhost",
-		port:8332,
-		username:"username",
-		password:"password"
+		port:8552,
+		username:"rpcuser",
+		password:"rpcpassword"
 	},
 
 	// optional: enter your api access key from ipstack.com below
